87354ea96f7d0505b6b843cd3bf472470485e989
Josh McLain <joshmclain45@gmail.com>
2023-02-16 00:50:48 -0700

    add license

--- src/components/BonusTracks.tsx
+++ src/components/BonusTracks.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const BonusTracks = (props: Props) => {
   return (
     <section id="bonusTracks">
-        <h3 className="text-4xl text-center">Bonus Tracks</h3>
-        <p className="text-md px-4">
-          How the hell did you get here?
-        </p>
-        
-      </section>
-  )
-}
+      <h3 className="text-4xl text-center">Bonus Tracks</h3>
+      <p className="text-md px-4">How the hell did you get here?</p>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/HUngLgGRJpo"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </section>
+  );
+};
 
-export default BonusTracks
+export default BonusTracks;
