d952906354226ffe5ceed8170f98ca4020b19134
Josh McLain <joshmclain45@gmail.com>
2023-02-20 21:50:36 -0700

    fix scss error from dev and build

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,11 @@ import svgrPlugin from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    port: 3000,
+    open: true,
+  }
 });
